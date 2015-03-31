c065f3f7cacb534f7f1c85de46c025b42d47ee72
Tacitus89 <marco.can@gmx.de>
2015-03-31 19:19:19 +0200

    Revert "Revert "Basics""
    This reverts commit f11c82661862876a897c9eb81c5726f73591d531.

---
+++ ext.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+*
+* @package GamesMod
+* @copyright (c) 2015
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace tacitus89\gamesmod;
+
+class ext extends \phpbb\extension\base
+{
+}
